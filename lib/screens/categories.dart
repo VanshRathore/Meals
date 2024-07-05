3f0af43a275513fb8c56c947d5320c97805af137
Vansh Rathore <73779153+VanshRathore@users.noreply.github.com>
2024-07-05 21:09:08 +0530

    first commit

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,6 +5,7 @@ import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,14 +16,14 @@ class CategoriesScreen extends StatelessWidget {
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 1.5,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisExtent: 20,
+          mainAxisSpacing: 20,
         ),
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-          for ( final category in availableCategories)
-          CategoryGridItem(category: category)
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
